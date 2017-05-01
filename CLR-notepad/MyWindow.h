3ea6d600f08909acee6daf9b39fe40692ea1caae
ShadowIterator <ShadowIterator@hotmail.com>
2017-05-01 16:12:33 +0800

    update lineno

--- CLR-notepad/MyWindow.h
+++ CLR-notepad/MyWindow.h
@@ -10,7 +10,7 @@ namespace CLRnotepad {
 	using namespace System::Drawing;
 	using namespace System::IO;
 	using namespace System::Text;
-
+//	using namespace System::Windows::Forms::Control;
 	/// <summary>
 	/// MyWindow 摘要
 	/// </summary>
@@ -62,7 +62,7 @@ namespace CLRnotepad {
 	private: System::Windows::Forms::ToolStripMenuItem^  保存SToolStripMenuItem;
 	private: System::Windows::Forms::ToolStripMenuItem^  退出ToolStripMenuItem;
 	private: System::Windows::Forms::TableLayoutPanel^  tableLayoutPanel1;
-	private: System::Windows::Forms::RichTextBox^  richTextBox1;
+
 	private: System::Windows::Forms::ToolStripMenuItem^  打开OToolStripMenuItem;
 	private: System::Windows::Forms::SaveFileDialog^  saveFileDialog1;
 
@@ -73,6 +73,8 @@ namespace CLRnotepad {
 	private: System::Windows::Forms::OpenFileDialog^  openFileDialog1;
 	private: System::Windows::Forms::ColorDialog^  colorDialog1;
 	private: System::Windows::Forms::ToolStripMenuItem^  colorToolStripMenuItem;
+	private: System::Windows::Forms::Panel^  panel1;
+	private: System::Windows::Forms::RichTextBox^  richTextBox1;
 
 
 
@@ -99,6 +101,7 @@ namespace CLRnotepad {
 			this->退出ToolStripMenuItem = (gcnew System::Windows::Forms::ToolStripMenuItem());
 			this->testToolStripMenuItem = (gcnew System::Windows::Forms::ToolStripMenuItem());
 			this->fontToolStripMenuItem = (gcnew System::Windows::Forms::ToolStripMenuItem());
+			this->colorToolStripMenuItem = (gcnew System::Windows::Forms::ToolStripMenuItem());
 			this->tableLayoutPanel1 = (gcnew System::Windows::Forms::TableLayoutPanel());
 			this->richTextBox1 = (gcnew System::Windows::Forms::RichTextBox());
 			this->saveFileDialog1 = (gcnew System::Windows::Forms::SaveFileDialog());
@@ -106,7 +109,7 @@ namespace CLRnotepad {
 			this->fontDialog1 = (gcnew System::Windows::Forms::FontDialog());
 			this->openFileDialog1 = (gcnew System::Windows::Forms::OpenFileDialog());
 			this->colorDialog1 = (gcnew System::Windows::Forms::ColorDialog());
-			this->colorToolStripMenuItem = (gcnew System::Windows::Forms::ToolStripMenuItem());
+			this->panel1 = (gcnew System::Windows::Forms::Panel());
 			this->menuStrip1->SuspendLayout();
 			this->tableLayoutPanel1->SuspendLayout();
 			this->SuspendLayout();
@@ -118,7 +121,7 @@ namespace CLRnotepad {
 			this->menuStrip1->Location = System::Drawing::Point(0, 0);
 			this->menuStrip1->Name = L"menuStrip1";
 			this->menuStrip1->Padding = System::Windows::Forms::Padding(8, 2, 0, 2);
-			this->menuStrip1->Size = System::Drawing::Size(379, 28);
+			this->menuStrip1->Size = System::Drawing::Size(768, 28);
 			this->menuStrip1->TabIndex = 0;
 			this->menuStrip1->Text = L"menuStrip1";
 			this->menuStrip1->ItemClicked += gcnew System::Windows::Forms::ToolStripItemClickedEventHandler(this, &MyWindow::menuStrip1_ItemClicked);
@@ -164,7 +167,7 @@ namespace CLRnotepad {
 			// testToolStripMenuItem
 			// 
 			this->testToolStripMenuItem->Name = L"testToolStripMenuItem";
-			this->testToolStripMenuItem->Size = System::Drawing::Size(181, 26);
+			this->testToolStripMenuItem->Size = System::Drawing::Size(136, 26);
 			this->testToolStripMenuItem->Text = L"test(&T)";
 			this->testToolStripMenuItem->Click += gcnew System::EventHandler(this, &MyWindow::testToolStripMenuItem_Click);
 			// 
@@ -174,6 +177,13 @@ namespace CLRnotepad {
 			this->fontToolStripMenuItem->Size = System::Drawing::Size(136, 26);
 			this->fontToolStripMenuItem->Text = L"font";
 			this->fontToolStripMenuItem->Click += gcnew System::EventHandler(this, &MyWindow::fontToolStripMenuItem_Click);
+			// 
+			// colorToolStripMenuItem
+			// 
+			this->colorToolStripMenuItem->Name = L"colorToolStripMenuItem";
+			this->colorToolStripMenuItem->Size = System::Drawing::Size(136, 26);
+			this->colorToolStripMenuItem->Text = L"color";
+			this->colorToolStripMenuItem->Click += gcnew System::EventHandler(this, &MyWindow::colorToolStripMenuItem_Click);
 			// 
 			// tableLayoutPanel1
 			// 
@@ -185,12 +195,12 @@ namespace CLRnotepad {
 			this->tableLayoutPanel1->ColumnStyles->Add((gcnew System::Windows::Forms::ColumnStyle(System::Windows::Forms::SizeType::Percent,
 				50)));
 			this->tableLayoutPanel1->Controls->Add(this->richTextBox1, 0, 0);
-			this->tableLayoutPanel1->Location = System::Drawing::Point(0, 35);
+			this->tableLayoutPanel1->Location = System::Drawing::Point(55, 31);
 			this->tableLayoutPanel1->Margin = System::Windows::Forms::Padding(4);
 			this->tableLayoutPanel1->Name = L"tableLayoutPanel1";
 			this->tableLayoutPanel1->RowCount = 1;
 			this->tableLayoutPanel1->RowStyles->Add((gcnew System::Windows::Forms::RowStyle(System::Windows::Forms::SizeType::Percent, 50)));
-			this->tableLayoutPanel1->Size = System::Drawing::Size(379, 291);
+			this->tableLayoutPanel1->Size = System::Drawing::Size(713, 692);
 			this->tableLayoutPanel1->TabIndex = 2;
 			// 
 			// richTextBox1
@@ -198,36 +208,41 @@ namespace CLRnotepad {
 			this->richTextBox1->Anchor = static_cast<System::Windows::Forms::AnchorStyles>((((System::Windows::Forms::AnchorStyles::Top | System::Windows::Forms::AnchorStyles::Bottom)
 				| System::Windows::Forms::AnchorStyles::Left)
 				| System::Windows::Forms::AnchorStyles::Right));
-			this->richTextBox1->EnableAutoDragDrop = true;
-			this->richTextBox1->Location = System::Drawing::Point(4, 4);
-			this->richTextBox1->Margin = System::Windows::Forms::Padding(4);
+			this->richTextBox1->Location = System::Drawing::Point(3, 3);
 			this->richTextBox1->Name = L"richTextBox1";
-			this->richTextBox1->Size = System::Drawing::Size(371, 283);
+			this->richTextBox1->Size = System::Drawing::Size(707, 686);
 			this->richTextBox1->TabIndex = 0;
-			this->richTextBox1->Text = L"Default";
+			this->richTextBox1->Text = L"default";
+			this->richTextBox1->VScroll += gcnew System::EventHandler(this, &MyWindow::richTextBox1_VScroll);
+			this->richTextBox1->TextChanged += gcnew System::EventHandler(this, &MyWindow::richTextBox1_TextChanged);
 			// 
 			// openFileDialog1
 			// 
 			this->openFileDialog1->FileName = L"openFileDialog1";
 			// 
-			// colorToolStripMenuItem
+			// panel1
 			// 
-			this->colorToolStripMenuItem->Name = L"colorToolStripMenuItem";
-			this->colorToolStripMenuItem->Size = System::Drawing::Size(181, 26);
-			this->colorToolStripMenuItem->Text = L"color";
-			this->colorToolStripMenuItem->Click += gcnew System::EventHandler(this, &MyWindow::colorToolStripMenuItem_Click);
+			this->panel1->Anchor = static_cast<System::Windows::Forms::AnchorStyles>(((System::Windows::Forms::AnchorStyles::Top | System::Windows::Forms::AnchorStyles::Bottom)
+				| System::Windows::Forms::AnchorStyles::Left));
+			this->panel1->Location = System::Drawing::Point(0, 31);
+			this->panel1->Name = L"panel1";
+			this->panel1->Size = System::Drawing::Size(48, 692);
+			this->panel1->TabIndex = 3;
+			this->panel1->Paint += gcnew System::Windows::Forms::PaintEventHandler(this, &MyWindow::panel1_Paint);
 			// 
 			// MyWindow
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(8, 15);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->ClientSize = System::Drawing::Size(379, 328);
+			this->ClientSize = System::Drawing::Size(768, 725);
+			this->Controls->Add(this->panel1);
 			this->Controls->Add(this->tableLayoutPanel1);
 			this->Controls->Add(this->menuStrip1);
 			this->MainMenuStrip = this->menuStrip1;
 			this->Margin = System::Windows::Forms::Padding(4);
 			this->Name = L"MyWindow";
 			this->Text = L"MyWindow";
+			this->Load += gcnew System::EventHandler(this, &MyWindow::MyWindow_Load);
 			this->menuStrip1->ResumeLayout(false);
 			this->menuStrip1->PerformLayout();
 			this->tableLayoutPanel1->ResumeLayout(false);
@@ -264,7 +279,7 @@ namespace CLRnotepad {
 		openFileDialog1->FilterIndex = 2;
 		openFileDialog1->RestoreDirectory = true;
 
-		unsigned char s[100];
+//		unsigned char s[100];
 
 		if (openFileDialog1->ShowDialog() == System::Windows::Forms::DialogResult::OK)
 		{
@@ -304,5 +319,101 @@ private: System::Void colorToolStripMenuItem_Click(System::Object^  sender, Syst
 		richTextBox1->SelectionColor = colorDialog1->Color;
 	}
 }
+
+		  private: void showLineNo()
+		  {
+			  //获得当前坐标信息
+			  Point p = richTextBox1->Location;
+			  int crntFirstIndex = richTextBox1->GetCharIndexFromPosition(p);
+			  int crntFirstLine = richTextBox1->GetLineFromCharIndex(crntFirstIndex);
+
+			  Point crntFirstPos = richTextBox1->GetPositionFromCharIndex(crntFirstIndex);
+
+			  p.Y += richTextBox1->Height;
+
+			  int crntLastIndex = richTextBox1->GetCharIndexFromPosition(p);
+
+			  int crntLastLine = richTextBox1->GetLineFromCharIndex(crntLastIndex);
+			  Point crntLastPos = richTextBox1->GetPositionFromCharIndex(crntLastIndex);
+
+			  //准备画图
+			  Graphics^ g = panel1->CreateGraphics();
+
+			  //	 Font^ fnt = gcnew Font(richTextBox1->Font, richTextBox1->Font->Style);
+			  //	 Font
+			  System::Drawing::Font^ font = gcnew System::Drawing::Font(richTextBox1->Font, richTextBox1->Font->Style);
+
+
+			  SolidBrush^ brush = gcnew SolidBrush(Color::Green);
+
+			  //画图开始
+
+			  //刷新画布
+
+			  Rectangle rect = panel1->ClientRectangle;
+			  brush->Color = panel1->BackColor;
+
+			  g->FillRectangle(brush, 0, 0, panel1->ClientRectangle.Width, panel1->ClientRectangle.Height);
+
+			  brush->Color = Color::Black;//重置画笔颜色
+
+										  //绘制行号
+
+			  int lineSpace = 0;
+
+			  if (crntFirstLine != crntLastLine)
+			  {
+				  lineSpace = (crntLastPos.Y - crntFirstPos.Y) / (crntLastLine - crntFirstLine);
+
+			  }
+
+			  else
+			  {
+				  lineSpace = Convert::ToInt32(richTextBox1->Font->Size);
+
+			  }
+
+			  int brushX = panel1->ClientRectangle.Width - Convert::ToInt32(font->Size * 3);
+
+			  int brushY = crntLastPos.Y + Convert::ToInt32(font->Size * 0.21f);//惊人的算法啊！！
+			  for (int i = crntLastLine; i >= crntFirstLine; i--)
+			  {
+
+				  g->DrawString((i + 1).ToString(), font, brush, brushX, brushY);
+
+				  brushY -= lineSpace;
+			  }
+
+			  //	 g->Dispose();
+
+			  //	 font->Dispose();
+
+			  //	 brush->Dispose();
+			  delete g;
+			  delete font;
+			  delete brush;
+		  }
+
+private: System::Void panel1_Paint(System::Object^  sender, System::Windows::Forms::PaintEventArgs^  e) {
+//	Graphics^ g = this->panel1->CreateGraphics();
+	showLineNo();
+}
+
+private: System::Void MyWindow_Load(System::Object^  sender, System::EventArgs^  e) {
+}
+private: System::Void richTextBox1_TextChanged(System::Object^  sender, System::EventArgs^  e) {
+	//panel1_Paint(sender, Null);
+	showLineNo();
+}
+private: System::Void richTextBox1_VScroll(System::Object^  sender, System::EventArgs^  e)
+{
+	showLineNo();
+}
+/*
+private: System::Void OnTextChanged(System::TextChangedEventArgs^ e)
+{
+
+}
+*/
 };
 }
